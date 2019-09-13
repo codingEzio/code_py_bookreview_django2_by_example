4270416aa59fa38e7202345580174ade9dcf6e76
Ezio <assassinste@gmail.com>
2019-09-13 23:36:54 +0800

    Generate a meaningful url for each inst of 'Post'

--- djbBlog/blog/models.py
+++ djbBlog/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
+
+from django.utils import timezone
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -44,3 +46,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
