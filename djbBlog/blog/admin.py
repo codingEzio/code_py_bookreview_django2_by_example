e2ec9b64cab64cfe015903c1e1515f3b25e29b20
Ezio <assassinste@gmail.com>
2019-09-16 17:07:05 +0800

    Creating model 'Comment' (=> admin, => form)

--- djbBlog/blog/admin.py
+++ djbBlog/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -18,3 +18,10 @@ class PostAdmin(admin.ModelAdmin):
 
     date_hierarchy = 'publish'  # behaves like filters I'd say (below the search bar)
     ordering = ('status', 'publish')  # default ordering rule
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
--- djbBlog/blog/forms.py
+++ djbBlog/blog/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from .models import Comment
+
 
 class EmailPostForm(forms.Form):
     """Each widget has its own validation rule (e.g. <textarea>, <input>)
@@ -9,3 +11,13 @@ class EmailPostForm(forms.Form):
     email_to = forms.EmailField()
     comments = forms.CharField(required=False,
                                widget=forms.Textarea)
+
+
+class CommentForm(forms.ModelForm):
+    """Since this form is mostly based on the `Comment` model,
+    we don't have to write every fields again, just name what we need is enough.
+    """
+
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
