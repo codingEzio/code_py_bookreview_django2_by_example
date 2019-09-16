98d48c69a5ca61998e60a6bb94b67ea7dc88072f
Ezio <assassinste@gmail.com>
2019-09-16 21:48:00 +0800

    List all posts tagged with a specific tag

--- djbBlog/blog/urls.py
+++ djbBlog/blog/urls.py
@@ -4,10 +4,16 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
-         views.post_detail,
-         name='post_detail'),
+    # path('', views.PostListView.as_view(), name='post_list'),
+    path('',
+         views.post_list, name='post_list'),
 
-    path('<int:post_id>/', views.post_share, name='post_share'),
+    path('tag/<slug:tag_slug>/',
+         views.post_list, name='post_list_by_tag'),
+
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_detail, name='post_detail'),
+
+    path('<int:post_id>/',
+         views.post_share, name='post_share'),
 ]
