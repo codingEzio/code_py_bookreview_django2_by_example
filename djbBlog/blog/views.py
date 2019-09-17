2c25e3de7752c34d7e2f07ce557214082cd905fe
Ezio <assassinste@gmail.com>
2019-09-17 18:28:19 +0800

    Tweaking search feature (adding "weight")

--- djbBlog/blog/views.py
+++ djbBlog/blog/views.py
@@ -7,7 +7,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.db.models import Count
 
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm, SearchForm
@@ -129,9 +129,15 @@ def post_search(request):
 
         if form.is_valid():
             query = form.cleaned_data['query']
+
+            search_vector = SearchVector('title', weight='A') + \
+                            SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+
             results = Post.posts \
-                .annotate(search=SearchVector('title', 'body')) \
-                .filter(search=query)
+                .annotate(search=search_vector, rank=SearchRank(search_vector, search_query)) \
+                .filter(rank__gte=0.3) \
+                .order_by('-rank')
 
     return render(request,
                   'blog/post/search.html',
