0bb406dfa9a0d7020082e1568ce6082bff40419d
Ezio <assassinste@gmail.com>
2019-09-24 21:57:05 +0800

    Adding model for activity-stream

--- djbBookmark/actions/models.py
+++ djbBookmark/actions/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
 
 class Action(models.Model):
     user = models.ForeignKey('auth.User',
@@ -9,6 +12,16 @@ class Action(models.Model):
     verb = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True,
                                    db_index=True)
+
+    target_ct = models.ForeignKey(ContentType,
+                                  blank=True,
+                                  null=True,
+                                  related_name='target_obj',
+                                  on_delete=models.CASCADE)
+    target_id = models.PositiveIntegerField(null=True,
+                                            blank=True,
+                                            db_index=True)
+    target = GenericForeignKey('target_ct', 'target_id')
 
     class Meta:
         ordering = ('-created',)
