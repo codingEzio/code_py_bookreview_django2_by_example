f10af90efa435fb3916a950212b2dd99ba857459
Ezio <assassinste@gmail.com>
2019-09-24 16:18:41 +0800

    Adding new app 'actions' (X Y-ed Z)

--- djbBookmark/djbBookmark/settings.py
+++ djbBookmark/djbBookmark/settings.py
@@ -26,6 +26,7 @@ ALLOWED_HOSTS = [
 INSTALLED_APPS = [
     'account.apps.AccountConfig',
     'images.apps.ImagesConfig',
+    'actions.apps.ActionsConfig',
 
     'django.contrib.admin',
     'django.contrib.auth',
