d87f2fdea772693452296a18431f6452a615c2da
Ezio <assassinste@gmail.com>
2019-09-24 23:21:55 +0800

    Two more techniques for boosting performance

--- djbBookmark/account/views.py
+++ djbBookmark/account/views.py
@@ -28,7 +28,10 @@ def dashboard(request):
 
     if following_ids:
         actions = actions.filter(user_id__in=following_ids)
-    actions = actions[:10]
+
+    actions = actions \
+                  .select_related('user', 'user__profile') \
+                  .prefetch_related('target')[:10]
 
     return render(request,
                   'account/dashboard.html',
