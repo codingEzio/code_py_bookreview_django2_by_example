60c6d2f4996d337f0401cb7383c57a31581ca44d
Ezio <assassinste@gmail.com>
2019-10-11 22:45:40 +0800

    Fixed typos

--- djbShop/shop/models.py
+++ djbShop/shop/models.py
@@ -15,7 +15,8 @@ class Category(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('shop:product_list_by_category', args=[self.slug])
+        return reverse(viewname='shop:product_list_by_category',
+                       args=[self.slug])
 
 
 class Product(models.Model):
@@ -41,4 +42,5 @@ class Product(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('shop:product_detail', args=[self.id, self.slug])
+        return reverse(viewname='shop:product_detail',
+                       args=[self.id, self.slug])
