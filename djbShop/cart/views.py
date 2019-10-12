fe3b232ad8cb72bd0b0d917b3765cd070d334244
Ezio <assassinste@gmail.com>
2019-10-12 22:11:10 +0800

    Adding generic operations for "Cart" (R,U,D)

--- djbShop/cart/views.py
+++ djbShop/cart/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from shop.models import Product
+from .cart import Cart
+from .forms import CartAddProductForm
+
+
+def cart_detial(request):
+    cart = Cart(request)
+
+    return render(request=request,
+                  template_name='cart/detail.html',
+                  context={'cart': cart})
+
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cleaned = form.cleaned_data
+        cart.add(product=product,
+                 quantity=cleaned['quantity'],
+                 initial_update_quantity=cleaned['update'])
+
+    return redirect(to='cart:cart_detail')
+
+
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+
+    cart.remove(product)
+
+    return redirect(to='cart:cart_detail')
