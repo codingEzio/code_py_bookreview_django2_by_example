fac80028b9b21aeec3e25407ab83540bc29bb531
Ezio <assassinste@gmail.com>
2019-10-12 20:02:55 +0800

    REFACTORING :: being as explicit as possible

--- djbShop/shop/views.py
+++ djbShop/shop/views.py
@@ -11,14 +11,18 @@ def product_list(request, category_slug=None):
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
 
-    return render(request, 'shop/product/list.html', {
-        'category': category,
-        'categories': categories,
-        'products': products
-    })
+    return render(request=request,
+                  template_name='shop/product/list.html',
+                  context={
+                      'category': category,
+                      'categories': categories,
+                      'products': products
+                  })
 
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
 
-    return render(request, 'shop/product/detail.html', {'product': product})
+    return render(request=request,
+                  template_name='shop/product/detail.html',
+                  context={'product': product})
