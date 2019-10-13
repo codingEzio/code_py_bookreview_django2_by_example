3bab501db490c09c91483c4ec86352d28370313a
Ezio <assassinste@gmail.com>
2019-10-13 11:39:40 +0800

    Adding checkout logic: create new order or not

--- djbShop/orders/views.py
+++ djbShop/orders/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import OrderItem
+from .forms import OrderCreateForm
+from cart.cart import Cart
+
+
+def order_create(request):
+    cart = Cart(request)
+
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+
+        if form.is_valid():
+            order = form.save()
+
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                         product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+            cart.clear()
+
+            return render(request=request,
+                          template_name='orders/order/created.html',
+                          context={'order': order})
+    else:
+        form = OrderCreateForm()
+
+    return render(request=request,
+                  template_name='orders/order/create.html',
+                  context={
+                      'cart': cart,
+                      'form': form
+                  })
