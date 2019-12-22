1962f01e29404f6410f226e5e0ada9776b5fc7b2
Ezio <assassinste@gmail.com>
2019-12-22 16:57:27 +0800

    [HALT] Opened another project for re-learning 'MyShop'

--- djbShop/orders/admin.py
+++ djbShop/orders/admin.py
@@ -1,5 +1,33 @@
+import csv
+import datetime
+
+from django.http import HttpResponse
 from django.contrib import admin
+
 from .models import Order, OrderItem
+
+
+def export_to_csv(model_admin, request, queryset):
+    opts = model_admin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = \
+        f'attachment;filename={opts.verbose_name}.csv'
+    writer = csv.writer(response)
+
+    fields = [
+        field for field in opts.get_fields()
+        if not field.many_to_many and not field.one_to_many
+    ]
+    writer.writerow([field.verbose_name for field in fields])
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
 
 
 class OrderItemInline(admin.TabularInline):
@@ -13,3 +41,6 @@ class OrderAdmin(admin.ModelAdmin):
                     'address', 'postal_code', 'created', 'updated')
     list_filter = ('paid', 'created', 'updated')
     inlines = [OrderItemInline]
+
+
+export_to_csv.description = 'Export to CSV'
