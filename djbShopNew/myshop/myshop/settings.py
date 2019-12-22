227662e6c82f136a1ffdd5bb1e16dcb4c772fc9e
Ezio <assassinste@gmail.com>
2019-12-22 18:26:29 +0800

    Project/app initialized, packages added, libraries configurated :)

---
+++ djbShopNew/myshop/myshop/settings.py
@@ -0,0 +1,131 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "4_s!_h@mqyxiyw2=wg+ol190@4rkx%g!(ch9irmurhzw^gv*f="
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[::1]"]
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    # shop
+    "shop.apps.ShopConfig",
+    # dev
+    "django_extensions",
+    "debug_toolbar",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+ROOT_URLCONF = "myshop.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
+        },
+    }
+]
+
+WSGI_APPLICATION = "myshop.wsgi.application"
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "shopdb.sqlite3"),
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+
+PASSWD_VALI_PREFIX = "django.contrib.auth.password_validation."
+AUTH_PASSWORD_VALIDATORS = [
+    {"NAME": f"{PASSWD_VALI_PREFIX}UserAttributeSimilarityValidator"},
+    {"NAME": f"{PASSWD_VALI_PREFIX}MinimumLengthValidator"},
+    {"NAME": f"{PASSWD_VALI_PREFIX}CommonPasswordValidator"},
+    {"NAME": f"{PASSWD_VALI_PREFIX}NumericPasswordValidator"},
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.0/topics/i18n/
+
+LANGUAGE_CODE = "en-us"
+
+USE_I18N = True
+USE_L10N = True
+
+TIME_ZONE = "UTC"
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+# Application-related files (CSS, JavaScript, Images etc.)
+STATIC_URL = "/static/"
+STATICFILESS_DIRS = [os.path.join(BASE_DIR, "static/")]
+
+# User-uploaded files (different from the purpose of 'STATIC_XXXX')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+
+
+# Email
+
+EMAIL_BACKEND = os.getenv("CURRENT_EMAIL_BACKEND")
+
+
+# Third-party libraries' configurations
+
+DEBUG_TOOLBAR_PANELS = [
+    "debug_toolbar.panels.versions.VersionsPanel",
+    "debug_toolbar.panels.timer.TimerPanel",
+    "debug_toolbar.panels.settings.SettingsPanel",
+    "debug_toolbar.panels.headers.HeadersPanel",
+    "debug_toolbar.panels.request.RequestPanel",
+    "debug_toolbar.panels.sql.SQLPanel",
+    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+    "debug_toolbar.panels.templates.TemplatesPanel",
+    "debug_toolbar.panels.cache.CachePanel",
+    "debug_toolbar.panels.signals.SignalsPanel",
+    "debug_toolbar.panels.logging.LoggingPanel",
+    "debug_toolbar.panels.redirects.RedirectsPanel",
+    "debug_toolbar.panels.profiling.ProfilingPanel",
+]
---
+++ djbShopNew/myshop/myshop/urls.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("shop.urls", namespace="shop")),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    # Third-party debug library
+    urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
+
+    # Media (user-uploaded) files
+    urlpatterns += static(
+        prefix=settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+
