a727ddf9d88fae284170908d75090d5de5cf1cf5
Ezio <assassinste@gmail.com>
2019-12-22 21:54:30 +0800

    Implement views & routes for produst list/detail

--- djbShopNew/myshop/myshop/urls.py
+++ djbShopNew/myshop/myshop/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("", include("shop.urls", namespace="shop")),
+    path("", include("shop.urls", namespace="shop")),
 ]
 
 if settings.DEBUG:
@@ -19,4 +19,3 @@ if settings.DEBUG:
     urlpatterns += static(
         prefix=settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
-
--- djbShopNew/myshop/shop/urls.py
+++ djbShopNew/myshop/shop/urls.py
@@ -4,3 +4,17 @@ from . import views
 
 app_name = "shop"
 
+
+urlpatterns = [
+    path(route="", view=views.product_list, name="product_list"),
+    path(
+        route="<slug:category_slug>/",
+        view=views.product_list,
+        name="product_list_by_category",
+    ),
+    path(
+        route="<int:id>/<slug:slug>/",
+        view=views.product_detail,
+        name="product_detail",
+    ),
+]
