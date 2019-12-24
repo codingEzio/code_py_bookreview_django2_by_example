529941f74264b78bb178a3f3a219524cbf63ed31
Ezio <assassinste@gmail.com>
2019-12-25 00:48:50 +0800

    Implement admin feature 'export orders to CSV'

--- djbShopNew/myshop/orders/admin.py
+++ djbShopNew/myshop/orders/admin.py
@@ -1,5 +1,51 @@
+import csv
+import datetime
 from django.contrib import admin
+from django.http import HttpResponse
+
 from .models import Order, OrderItem
+
+
+def export_to_csv(modeladmin, request, queryset):
+    """
+    Don't let these code frighten you, it's actually just two steps,
+    get the field name & its correspond data, and then write it to a file.
+    """
+
+    # The core of the Django ORM. It enables other parts of
+    # the system to understand the capabilities of each model.
+    options = modeladmin.model._meta
+
+    response = HttpResponse(content_type="text/csv")
+    response[
+        "Content-Disposition"
+    ] = f"attachment;filename={options.verbose_name}.csv"
+    writer = csv.writer(response)
+
+    # A list of '<django.db.models.fields.XxxField: field_name>' strings
+    model_fields = [
+        field
+        for field in options.get_fields()
+        if not field.many_to_many and not field.one_to_many
+    ]
+
+    # The header
+    writer.writerow([field.verbose_name for field in model_fields])
+
+    # The data rows
+    for obj in queryset:
+        data_row = []
+        for field in model_fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime("%d/%m/%Y")
+            data_row.append(value)
+        writer.writerow(data_row)
+
+    return response
+
+
+export_to_csv.short_description = "Export to CSV"
 
 
 class OrderItemInline(admin.TabularInline):
@@ -10,6 +56,7 @@ class OrderItemInline(admin.TabularInline):
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
 
     list_display = [
         "id",
