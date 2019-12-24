77d19bfb9be917320db59f108782b26e06c77f0b
Ezio <assassinste@gmail.com>
2019-12-24 20:29:42 +0800

    Implement method 'order_create' (does exactly what its name suggests)

--- djbShopNew/myshop/orders/views.py
+++ djbShopNew/myshop/orders/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .cart import CartSession as SessionCart
+from .models import Order, OrderItem
+from .forms import OrderCreateForm
+
+
+def order_create(request):
+    cart_session = SessionCart(request)
+
+    form = OrderCreateForm(request.POST)
+    if form.is_valid():
+        # The 'Order' part
+        order_table_inst = form.save()
+
+        # The 'OrderItem' part
+        for cart_inst in cart_session:
+            OrderItem.objects.create(
+                order=order_table_inst,
+                product=cart_inst["product"],
+                price=cart_inst["price"],
+                quantity=cart_inst["quantity"],
+            )
+
+        cart_session.clear()
+        return render(
+            request=request,
+            template_name="orders/order/created.html",
+            context={"order_table_inst": order_table_inst},
+        )
+    else:
+        form = OrderCreateForm()
+
+    return render(
+        request=request,
+        template_name="orders/order/create.html",
+        context={"cart_session": cart_session, "form": form},
+    )
