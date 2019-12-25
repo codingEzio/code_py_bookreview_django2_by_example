00e903c0b9a2f758f94bb372feeef605ca3958b4
Ezio <assassinste@gmail.com>
2019-12-25 23:26:33 +0800

    Models added & registered: [Coupon] (2nd mig was to fix a typo)

--- djbShopNew/myshop/coupons/admin.py
+++ djbShopNew/myshop/coupons/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Coupon
 
-# Register your models here.
+
+class CouponAdmin(admin.ModelAdmin):
+    list_display = [
+        "coupon_code",
+        "valid_from",
+        "valid_to",
+        "discount",
+        "active",
+    ]
+    list_filter = ["active", "valid_from", "valid_to"]
+    search_fields = ["coupon_code"]
+
+
+admin.site.register(Coupon, CouponAdmin)
--- djbShopNew/myshop/coupons/models.py
+++ djbShopNew/myshop/coupons/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Coupon(models.Model):
+    coupon_code = models.CharField(max_length=50, unique=True)
+    active = models.BooleanField(default=False)
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+    discount = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
+
+    def __str__(self):
+        return self.coupon_code
