4d6c89fe13a99520e0938560550d8af389e0aba1
Ezio <assassinste@gmail.com>
2019-12-26 20:41:50 +0800

    Implement 'coupons' :: behind-the-scenes processing based on 'coupon_id'

--- djbShopNew/myshop/cart/cart.py
+++ djbShopNew/myshop/cart/cart.py
@@ -1,6 +1,8 @@
 from decimal import Decimal
 from django.conf import settings
+
 from shop.models import Product
+from coupons.models import Coupon
 
 
 class Cart(object):
@@ -29,6 +31,7 @@ class Cart(object):
             cart = self.session[settings.CART_SESSION_ID] = {}
 
         self.cart = cart
+        self.coupon_id = self.session.get("coupon_id")
 
     def __iter__(self):
         """
@@ -60,11 +63,27 @@ class Cart(object):
         #   https://docs.djangoproject.com/en/3.0/topics/http/sessions/#when-sessions-are-saved
         self.session.modified = True
 
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            return Coupon.objects.get(id=self.coupon_id)
+        return None
+
+    def get_discount(self):
+        if self.coupon:
+            return (
+                self.coupon.discount / Decimal("100")
+            ) * self.get_total_price()
+        return Decimal('0')
+
     def get_total_price(self):
         return sum(
             Decimal(item["price"]) * item["quantity"]
             for item in self.cart.values()
         )
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
 
     def add(self, product, quantity, update_quantity=False):
         """
