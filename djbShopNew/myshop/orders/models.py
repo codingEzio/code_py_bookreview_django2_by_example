5650d49b01b324b3a5e0cec3d484312d27808682
Ezio <assassinste@gmail.com>
2019-12-27 20:13:55 +0800

    Translating my order fields

--- djbShopNew/myshop/orders/models.py
+++ djbShopNew/myshop/orders/models.py
@@ -1,18 +1,27 @@
 from decimal import Decimal
+
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from shop.models import Product
 from coupons.models import Coupon
 
 
 class Order(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    address = models.CharField(max_length=250)
-    postal_code = models.CharField(max_length=25)
-    city = models.CharField(max_length=100)
+    """
+    Since 'verbose_name' is the 1st arg of 'Field' (parent class of Char..),
+    you COULD write it like this: 'CharField(_("first_name"), max_length=50)'.
+    """
+
+    first_name = models.CharField(verbose_name=_("first_name"), max_length=50)
+    last_name = models.CharField(verbose_name=_("last_name"), max_length=50)
+    email = models.EmailField(verbose_name=_("email"))
+    address = models.CharField(verbose_name=_("address"), max_length=250)
+    postal_code = models.CharField(
+        verbose_name=_("postal_code"), max_length=25
+    )
+    city = models.CharField(verbose_name=_("city"), max_length=100)
     paid = models.BooleanField(default=False)
     discount = models.IntegerField(
         default=0, validators=[MinValueValidator(0), MaxValueValidator(100)]
