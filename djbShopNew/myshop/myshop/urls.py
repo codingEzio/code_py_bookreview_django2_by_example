6181e171c7b90d98beb45a1f6f3b1e5885b40602
Ezio <assassinste@gmail.com>
2019-12-28 16:08:49 +0800

    i18n for URL patterns: add a language prefix (/PREFIX/)

--- djbShopNew/myshop/myshop/urls.py
+++ djbShopNew/myshop/myshop/urls.py
@@ -1,22 +1,25 @@
-from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
+from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
+# Add a prefix to all the routes (one way of i18nize URLs)
+# Examples:
+#   http://localhost:8000/de/
+#   http://localhost:8000/en-gb/
+#   http://localhost:8000/zh-hant/
+urlpatterns = i18n_patterns(
     path(route="admin/", view=admin.site.urls),
     path(route="cart/", view=include(arg="cart.urls", namespace="cart")),
+    path(route="orders/", view=include(arg="orders.urls", namespace="orders")),
     path(
-        route="orders/", view=include(arg="orders.urls", namespace="orders")
+        route="coupons/", view=include(arg="coupons.urls", namespace="coupons")
     ),
-    path(
-        route="coupons/",
-        view=include(arg="coupons.urls", namespace="coupons"),
-    ),
-    path(route='rosetta/', view=include(arg='rosetta.urls')),
+    path(route="rosetta/", view=include(arg="rosetta.urls")),
     path(route="", view=include("shop.urls", namespace="shop")),
-]
+)
 
 if settings.DEBUG:
     import debug_toolbar
